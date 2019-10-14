59e78e1d774fa9febfa00a017de20f98841e25d9
Elijah Cobb <elijah@elijahcobb.com>
2019-10-13 21:21:21 -0400

    animated front page

---
+++ ts/index.ts
@@ -0,0 +1,83 @@
+/**
+ *
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * https://elijahcobb.com
+ *
+ */
+
+let imageUrls: string[] = [
+	"http://abadi.me.mtu.edu/wp-content/uploads/2017/12/photoshopped.jpg",
+	"http://abadi.me.mtu.edu/wp-content/uploads/2017/12/converted21-2.jpg",
+	"http://abadi.me.mtu.edu/wp-content/uploads/2019/08/IMG_5946.jpg",
+	"http://abadi.me.mtu.edu/wp-content/uploads/2017/12/converted21-1.jpg",
+	"https://unsplash.com/photos/lQGJCMY5qcM/download?force=true",
+	"https://unsplash.com/photos/pwcKF7L4-no/download?force=true",
+	"https://unsplash.com/photos/gKUC4TMhOiY/download?force=true"
+];
+
+let titles: string[] = [
+	"Cutting Edge",
+	"Research",
+	"Discovery",
+	"Innovation"
+];
+
+let backgroundImage: HTMLElement | null = document.getElementById("backgroundImage");
+let titleLabel: HTMLElement | null = document.getElementById("subtitleLabel");
+
+function getCSSForImageUrl(imageUrl: string): string {
+
+	return `radial-gradient(circle, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.75), black), url("${imageUrl}") no-repeat center`;
+
+}
+
+let imageIndex: number = Math.floor(Math.random() * imageUrls.length) - 1;
+if (imageIndex < 0) imageIndex = 0;
+
+let titleIndex: number = Math.floor(Math.random() * titles.length) - 1;
+if (titleIndex < 0) titleIndex = 0;
+
+function cycleBackgroundView(): void {
+
+	const imageUrl = imageUrls[imageIndex];
+	const imageCSS = getCSSForImageUrl(imageUrl);
+	if (backgroundImage == null) return;
+	backgroundImage.style.opacity = "0";
+
+	if (titleLabel == null) return;
+	titleLabel.style.opacity = "0";
+
+	setTimeout(() => {
+
+		if (backgroundImage == null) return;
+		backgroundImage.style.background = imageCSS;
+
+		if (titleLabel == null) return;
+		titleLabel.innerHTML = titles[titleIndex];
+
+		setTimeout(() => {
+
+			if (backgroundImage == null) return;
+			backgroundImage.style.opacity = "1";
+
+			if (titleLabel == null) return;
+			titleLabel.style.opacity = "1";
+
+			imageIndex++;
+			titleIndex++;
+
+			if (imageIndex >= imageUrls.length) imageIndex = 0;
+			if (titleIndex >= titles.length) titleIndex = 0;
+
+			setTimeout(cycleBackgroundView, 4000);
+
+		}, 500);
+
+	}, 500);
+
+
+
+}
+
+cycleBackgroundView();
